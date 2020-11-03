f8243916e4fd5120f21c35526554ecc0ea5aee45
yihau.chen <a122092487@gmail.com>
2020-11-03 22:54:08 +0800

    feat: add constructor init

--- contracts/Multisign.sol
+++ contracts/Multisign.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.6.0;
 contract Multisign {
     event ReceiveETH(address indexed sender, uint256 indexed num);
 
-    constructor() public {}
+    uint8 public requireMinimum;
+    mapping(address => bool) public owners;
+
+    constructor(address[] memory initOwners, uint8 initMinimumRequired) public {
+        require(
+            initOwners.length >= initMinimumRequired,
+            "owners count should greater than or equal to require minimum"
+        );
+        for (uint256 i = 0; i < initOwners.length; i++) {
+            owners[initOwners[i]] = true;
+        }
+        requireMinimum = initMinimumRequired;
+    }
 
     receive() external payable {
         emit ReceiveETH(msg.sender, msg.value);
--- test/mutisign.js
+++ test/mutisign.js
@@ -2,26 +2,50 @@ const { use, expect } = require("chai")
 const { solidity } = require("ethereum-waffle")
 use(solidity)
 
-describe("ETH", () => {
-  let multisign
-  let addr1
-  let addr2
-  let addr3
-  let addr4
+describe("Multisign", () => {
 
-  beforeEach(async function () {
-    [addr1, addr2, addr3, addr4] = await ethers.getSigners()
-    multisign = await (await ethers.getContractFactory("Multisign")).deploy()
+  describe("Contract Init", () => {
+    it("should revert when require minimum greater than owners count", async () => {
+      [addr1, addr2, addr3] = await ethers.getSigners()
+      initOwners = [addr1.address, addr2.address, addr3.address]
+      initRequireMinimum = 10
+      await expect((await ethers.getContractFactory("Multisign")).deploy(initOwners, initRequireMinimum)).
+        to.be.revertedWith("owners count should greater than or equal to require minimum")
+    })
+    it("should match between contract variable and init args", async () => {
+      [addr1, addr2, addr3, addr4] = await ethers.getSigners()
+      initOwners = [addr1.address, addr2.address, addr3.address]
+      initRequireMinimum = 2
+      multisign = await (await ethers.getContractFactory("Multisign")).deploy(initOwners, initRequireMinimum)
+      expect(await multisign.owners(addr1.address)).to.be.equal(true)
+      expect(await multisign.owners(addr2.address)).to.be.equal(true)
+      expect(await multisign.owners(addr3.address)).to.be.equal(true)
+      expect(await multisign.owners(addr4.address)).to.be.equal(false)
+      expect(await multisign.requireMinimum()).to.be.equal(initRequireMinimum)
+    })
   })
 
-  it("should success when send eth to contract", async () => {
-    await expect(() => addr1.sendTransaction({ to: multisign.address, value: 10 })).
-      to.be.changeEtherBalances([addr1, multisign], [-10, 10])
-  })
+  describe("ETH", () => {
+    let multisign
+    let addr1
+    let addr2
+    let addr3
+    let addr4
 
-  it("should emit event when send eth to contract", async () => {
-    await expect(addr1.sendTransaction({ to: multisign.address, value: 10 }))
-      .to.emit(multisign, 'ReceiveETH')
-      .withArgs(addr1.address, 10)
+    beforeEach(async function () {
+      [addr1, addr2, addr3, addr4] = await ethers.getSigners()
+      multisign = await (await ethers.getContractFactory("Multisign")).deploy([addr1.address, addr2.address, addr3.address], 2)
+    })
+
+    it("should success when send eth to contract", async () => {
+      await expect(() => addr1.sendTransaction({ to: multisign.address, value: 10 })).
+        to.be.changeEtherBalances([addr1, multisign], [-10, 10])
+    })
+
+    it("should emit event when send eth to contract", async () => {
+      await expect(addr1.sendTransaction({ to: multisign.address, value: 10 }))
+        .to.emit(multisign, 'ReceiveETH')
+        .withArgs(addr1.address, 10)
+    })
   })
 })
